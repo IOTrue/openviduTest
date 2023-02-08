72281a9088a7895d3f588935883ddae650c7d257
IOTrue <kjoonc.dev@gmail.com>
2023-02-08 20:08:19 +0900

    react proj 전환

--- openvidu-react/src/App.js
+++ openvidu-react/src/App.js
@@ -5,8 +5,7 @@ import React, { Component } from 'react';
 import './App.css';
 import UserVideoComponent from './UserVideoComponent';
 
-const APPLICATION_SERVER_URL = "https://iamhyunjun.shop:5000/";
-
+const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? '' : 'https://demos.openvidu.io/';
 
 class App extends Component {
     constructor(props) {
@@ -277,18 +276,19 @@ class App extends Component {
                                 onClick={this.leaveSession}
                                 value="Leave session"
                             />
+                            <input
+                                className="btn btn-large btn-success"
+                                type="button"
+                                id="buttonSwitchCamera"
+                                onClick={this.switchCamera}
+                                value="Switch Camera"
+                            />
                         </div>
 
                         {this.state.mainStreamManager !== undefined ? (
                             <div id="main-video" className="col-md-6">
                                 <UserVideoComponent streamManager={this.state.mainStreamManager} />
-                                <input
-                                    className="btn btn-large btn-success"
-                                    type="button"
-                                    id="buttonSwitchCamera"
-                                    onClick={this.switchCamera}
-                                    value="Switch Camera"
-                                />
+
                             </div>
                         ) : null}
                         <div id="video-container" className="col-md-6">
@@ -299,7 +299,8 @@ class App extends Component {
                                 </div>
                             ) : null}
                             {this.state.subscribers.map((sub, i) => (
-                                <div key={i} className="stream-container col-md-6 col-xs-6" onClick={() => this.handleMainVideoStream(sub)}>
+                                <div key={sub.id} className="stream-container col-md-6 col-xs-6" onClick={() => this.handleMainVideoStream(sub)}>
+                                    <span>{sub.id}</span>
                                     <UserVideoComponent streamManager={sub} />
                                 </div>
                             ))}
@@ -317,12 +318,12 @@ class App extends Component {
      * --------------------------------------------
      * The methods below request the creation of a Session and a Token to
      * your application server. This keeps your OpenVidu deployment secure.
-     * 
+     *
      * In this sample code, there is no user control at all. Anybody could
      * access your application server endpoints! In a real production
      * environment, your application server must identify the user to allow
      * access to the endpoints.
-     * 
+     *
      * Visit https://docs.openvidu.io/en/stable/application-server to learn
      * more about the integration of OpenVidu in your application server.
      */
